20fb63d5e3c988b3b59cc05fd5c6a7db0ae25be9
Nhật Huy <89715333+nhathuy157@users.noreply.github.com>
2024-10-17 17:25:32 +0700

    update
    update

--- index.js
+++ index.js
@@ -222,7 +222,3 @@ var formatCurrentcy = amount => new Intl.NumberFormat('vi-VN', { style: 'currenc
     });
 })();
 
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-  console.log('Hello world listening on port', port);
-});
